9736d389950998376a93a89f63cd04077409e9f6
emichaf <michael.frick@ericsson.com>
2018-02-11 17:33:44 +0100

    add test controller

---
+++ src/main/java/mytest/MyTest.java
@@ -0,0 +1,49 @@
+package mytest;
+
+import com.google.gson.JsonElement;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping("/*")
+public class MyTest {
+
+    RestTemplate restTemplate = new RestTemplate();
+    public void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    @RequestMapping(value = "/producer/msg", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity send(@RequestBody JsonElement body) {
+
+
+        String url = "http://docker104-eiffel999.lmera.ericsson.se:8082/job/test3/build";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
+        params.add("json", "{\"parameter\": [{\"name\":\"PARAM1\", \"value\":'{\"metatags\":\"my_metatags\"}'}]}");
+
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<MultiValueMap<String, String>>(params, headers);
+
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+
+        return response;
+    }
+
+}
+
+
+
+
